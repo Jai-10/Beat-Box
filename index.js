8ce522a866c5bddc3fe5c139a643624b18fc8e33
Jai-10 <71919287+Jai-10@users.noreply.github.com>
2020-12-25 12:30:37 +0530

    Added the necessary files

---
+++ index.js
@@ -0,0 +1,60 @@
+function show() {
+    var show_hide = document.getElementById("control-bar");
+    if (show_hide.style.display == "none") {
+        show_hide.style.display = "block";
+    }
+    else {
+        show_hide.style.display = "none";
+    }
+}
+
+
+// menu for queue
+function show_menu() {
+    var show_hide = document.getElementById("queue-menu");
+    if (show_hide.style.display == "none") {
+        show_hide.style.display = "block";
+    }
+    else {
+        show_hide.style.display = "none";
+    }
+}
+
+
+// menu for latest release
+function show_menu1() {
+    var show_hide = document.getElementById("latest-menu1");
+    if (show_hide.style.display == "none") {
+        show_hide.style.display = "block";
+    }
+    else {
+        show_hide.style.display = "none";
+    }
+}
+function show_menu2() {
+    var show_hide = document.getElementById("latest-menu2");
+    if (show_hide.style.display == "none") {
+        show_hide.style.display = "block";
+    }
+    else {
+        show_hide.style.display = "none";
+    }
+}
+function show_menu3() {
+    var show_hide = document.getElementById("latest-menu3");
+    if (show_hide.style.display == "none") {
+        show_hide.style.display = "block";
+    }
+    else {
+        show_hide.style.display = "none";
+    }
+}
+function show_menu4() {
+    var show_hide = document.getElementById("latest-menu4");
+    if (show_hide.style.display == "none") {
+        show_hide.style.display = "block";
+    }
+    else {
+        show_hide.style.display = "none";
+    }
+}
